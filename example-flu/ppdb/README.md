a6155ddf1d41cd3daad10694b1f9455b5256475b
Simone Conti <simonecnt@gmail.com>
2022-01-14 15:37:24 -0500

    Update influenza ppdb with FI sequences and IC50

---
+++ example-flu/ppdb/README.md
@@ -0,0 +1,12 @@
+
+Sequences for the antibodies are taken from:
+
+Kallewaard:
+ - https://doi.org/10.1016/j.cell.2016.05.073
+ - https://www.ncbi.nlm.nih.gov/popset/1044673543
+
+Corti:
+ - https://www.science.org/doi/10.1126/science.1205669
+ - https://www.ncbi.nlm.nih.gov/popset/342674570
+ - https://www.ncbi.nlm.nih.gov/popset/342674590
+
--- example-flu/ppdb/make-seq.py
+++ example-flu/ppdb/make-seq.py
@@ -15,20 +15,30 @@ def main():
     # Read sequences and database
     sequences_ag = ppdg.tools.read_multi_fasta('ag-cleaned.seq')
     sequences_ab = ppdg.tools.read_multi_fasta('kallewaard.seq')
+    sequences_ab.update(ppdg.tools.read_multi_fasta('corti.seq'))
+
+    # Check that the length of the nucleotide sequence is a multiple of 3
+    # Some VH sequences had an extra G at the end
+    # Some VK sequences had an extra C at the end
+    # Those have been modified manually in the sequence files
+    for k,v in sequences_ab.items():
+        if len(v)%3!=0:
+            raise ValueError('Nucleotide sequence length for %s is not a multiple of 3 (%d%%3=%d)' % (k, len(v), len(v)%3))
 
     # Prepare list of what to compute
     inputs = list()
-    with open('ppdb.txt') as fp:
-        for line in fp:
-            if line[0]=='#':
-                continue
-            name, recn, lign, dgexp, tpl = line.split()
-            ab, ag = name.split('__')
-            ab = ab.replace('_', '-')
-            sequence = sequences_ag[ag] + '/'
-            sequence = sequence*3
-            sequence = sequence + seq_nt2aa(sequences_ab[ab+'-VH']) + '/' + seq_nt2aa(sequences_ab[ab+'-VK'])
-            print(">%s\n%s\n" % (name, sequence))
+    with open('ppdb.txt', 'r') as fpin:
+        with open('ppdb.seq', 'w') as fpout:
+            for line in fpin:
+                if line[0]=='#':
+                    continue
+                name, recn, lign, dgexp, tpl = line.split()
+                ab, ag = name.split('__')
+                ab = ab.replace('_', '-')
+                sequence = sequences_ag[ag] + '/'
+                sequence = sequence*3
+                sequence = sequence + seq_nt2aa(sequences_ab[ab+'-VH']) + '/' + seq_nt2aa(sequences_ab[ab+'-VK'])
+                fpout.write(">%s\n%s\n" % (name, sequence))
 
 main()
 
