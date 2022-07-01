4dfad5c41ed1230ce907f167b11a997be7b73114
Simone Conti <simonecnt@gmail.com>
2022-07-01 14:02:34 -0400

    Fix link_data in case the link already exists but is broken

--- python/ppdx/__init__.py
+++ python/ppdx/__init__.py
@@ -13,12 +13,9 @@ import os
 import pkg_resources
 def get_data_fname(fname):
     return pkg_resources.resource_filename(__package__, os.path.join('data', fname))
-def link_data(fname, overwrite=False):
-    if os.path.isfile(fname):
-        if overwrite:
-            os.remove(fname)
-        else:
-            return
+def link_data(fname, overwrite=True):
+    if os.path.lexists(fname):
+        os.remove(fname)
     source = pkg_resources.resource_filename(__package__, os.path.join('data', fname))
     if not os.path.isfile(source):
         raise ValueError('File %s does not exist! Cannot link it...' % (source))
